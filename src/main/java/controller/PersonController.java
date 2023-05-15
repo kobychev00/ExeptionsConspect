be4db4b22a7e910f11c726ca55fd9c87a620abc6
air <kobychev00@bk.ru>
2023-05-15 19:58:28 +0300

    https://github.com/kobychev00/ExeptionsConspect

---
+++ src/main/java/controller/PersonController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class PersonController {
+    @GetMapping(path = "/person")
+
+}
---
+++ src/main/java/example/RunTimeExceptionExample.java
@@ -0,0 +1,10 @@
+package example;
+
+public class RunTimeExceptionExample {
+    public static void main(String[] args) {
+        int number = 3;
+        if (number != 2) {
+            throw new RuntimeException("Number is not 2");
+        }
+    }
+}
